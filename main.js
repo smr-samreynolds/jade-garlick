0c55c642a793dbb693c151c6fa37c44db7c61008
Samantha Reynolds <smr.samreynolds@gmail.com>
2024-04-11 16:39:29 +1000

    Adding JQuery

---
+++ main.js
@@ -0,0 +1,42 @@
+$(".slider-container").slick({
+  // Setting name: setting-value
+});
+
+$(".testimon-slider-container").slick({
+  autoplay: true, // Do we want it to autoplay? true or false
+  autoplaySpeed: 3000, // How long between each slide when autoplaying
+  speed: 500, // How fast is the transition
+  arrows: false, // Do you want to show arrows to trigger each slide
+  accessibility: true, // Enables tabbing and arrow key navigation
+  dots: true, // Enables the dots below to show how many slides
+  fade: false, // Changes the animate from slide to fade if true
+  infinite: true, // When true, means that it will scroll in a circle
+  pauseOnHover: true, // When true means the autoplay pauses when hovering
+  pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+});
+
+$(".testimonial-slider-container").slick({
+  autoplay: true,
+  autoplaySpeed: 2e3,
+  speed: 500,
+  arrows: true,
+  accessibility: true,
+  dots: true,
+  fade: false,
+  infinite: false,
+  pauseOnHover: false,
+  pauseOnDotsHover: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+  ],
+});
