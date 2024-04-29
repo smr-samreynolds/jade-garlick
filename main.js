43179f9f61452918dd2c54401ee351480b197c21
Samantha Reynolds <smr.samreynolds@gmail.com>
2024-04-29 20:35:57 +1000

    Homepage

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ $(".testimonial-slider-container").slick({
   autoplay: true, // Do we want it to autoplay? true or false
   autoplaySpeed: 3000, // How long between each slide when auto-playing
   speed: 500, // How fast is the transition in milliseconds
-  adaptiveHeight: true,
-  arrows: true, // Do you want to show arrows to trigger each slide
+  adaptiveHeight: false,
+  arrows: false, // Do you want to show arrows to trigger each slide
   accessibility: true, // Enables keyboard tabbing and arrow key navigation
-  dots: true, // Enables the dots below to show how many slides
+  dots: false, // Enables the dots below to show how many slides
   fade: false, // Changes the animate from slide to fade if true
   infinite: true, // When true, means that it will scroll in a circle
   pauseOnHover: true, // When true means the autoplay pauses when hovering
