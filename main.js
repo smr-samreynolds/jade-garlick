1ab0a63a7dbc4e96513bce3760ed48e0721d96fb
Samantha Reynolds <smr.samreynolds@gmail.com>
2024-05-20 16:56:54 +1000

    About page

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ $(".testimonial-slider-container").slick({
   pauseOnHover: true, // When true means the autoplay pauses when hovering
   pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
 });
+
+// initialise the script
+$(".image-slider-container").slick({
+  autoplay: true, // Do we want it to autoplay? true or false
+  autoplaySpeed: 2000, // How long between each slide when auto-playing
+  speed: 2000, // How fast is the transition in milliseconds
+  arrows: false, // Do you want to show arrows to trigger each slide
+  accessibility: true, // Enables keyboard tabbing and arrow key navigation
+  dots: false, // Enables the dots below to show how many slides
+  fade: true, // Changes the animate from slide to fade if true
+  infinite: true, // When true, means that it will scroll in a circle
+  pauseOnHover: false, // When true means the autoplay pauses when hovering
+  pauseOnDotsHover: false, // Pauses the autoplay when hovering over the dots
+});
