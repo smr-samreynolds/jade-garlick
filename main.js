73721708b967800b6e79893d6151264a782ad42d
Samantha Reynolds <smr.samreynolds@gmail.com>
2024-06-04 16:29:49 +1000

    Updates June 4 2024

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+// Sticky navbar on scroll
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
+});
+
 $(".testimonial-slider-container").slick({
   autoplay: true, // Do we want it to autoplay? true or false
   autoplaySpeed: 3000, // How long between each slide when auto-playing
